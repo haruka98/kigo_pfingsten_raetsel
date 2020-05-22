31c7cdb538b52bbb07dc313b1b056ed1581fe42e
Haruka <45176998+haruka98@users.noreply.github.com>
2020-05-22 19:06:38 +0200

    added basic text setup (missing code table)

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="de-DE">
+<head>
+<meta charset="utf-8" />
+<meta name="language" content="de"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="blue" />
+<title>KiGo Rätsel: Pfingsten</title>
+</head>
+
+<body style="text-align: center;">
+	<?php
+		if(array_key_exists('submit', $_POST)) {
+			if($_POST["a"] == "W" && $_POST["b"] == "U" && $_POST["c"] == "R" && $_POST["d"] == "D" && $_POST["e"] == "E" && $_POST["f"] == "N"
+			 && $_POST["g"] == "A" && $_POST["h"] == "N"
+			 && $_POST["i"] == "D" && $_POST["j"] == "I" && $_POST["k"] == "E" && $_POST["l"] == "S" && $_POST["m"] == "E" && $_POST["n"] == "M"
+			 && $_POST["o"] == "T" && $_POST["p"] == "A" && $_POST["q"] == "G"
+			 && $_POST["r"] == "I" && $_POST["s"] == "N"
+			 && $_POST["t"] == "D" && $_POST["u"] == "I" && $_POST["v"] == "E"
+			 && $_POST["w"] == "G" && $_POST["x"] == "E" && $_POST["y"] == "M" && $_POST["z"] == "E" && $_POST["aa"] == "I" && $_POST["ab"] == "N" && $_POST["ac"] == "D" && $_POST["ad"] == "E"
+			 && $_POST["ae"] == "A" && $_POST["af"] == "U" && $_POST["ag"] == "F" && $_POST["ah"] == "G" && $_POST["ai"] == "E" && $_POST["aj"] == "N" && $_POST["ak"] == "O" && $_POST["al"] == "M" && $_POST["am"] == "M" && $_POST["an"] == "E" && $_POST["ao"] == "N") {
+				echo "
+				<script type=\"text/javascript\" language=\"Javascript\">
+					alert(\"Super! Du hast alles richtig gelöst.\");
+				</script>";
+			} else {
+				echo "
+				<script type=\"text/javascript\" language=\"Javascript\">
+					alert(\"Leider nicht ganz richtig.\");
+				</script>";
+			}
+		}
+	?>
+	<h2>Vervollständige den folgenden Satz:</h2>
+	<br />
+	<h2>Etwa 3000 Menschen...</h2>
+	<table>
+		<tr>
+			<th><input name="a" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="b" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="c" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="d" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="e" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="f" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+		</tr>
+		<tr>
+			<th>92</th>
+			<th>91</th>
+			<th>72</th>
+			<th>32</th>
+			<th>41</th>
+			<th>62</th>
+		</tr>
+	</table>
+	<table>
+		<tr>
+			<th><input name="g" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="h" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+		</tr>
+		<tr>
+			<th>31</th>
+			<th>62</th>
+		</tr>
+	</table>
+	<table>
+		<tr>
+			<th><input name="i" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="j" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="k" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="l" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="m" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="n" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+		</tr>
+		<tr>
+			<th>32</th>
+			<th>52</th>
+			<th>41</th>
+			<th>81</th>
+			<th>41</th>
+			<th>61</th>
+		</tr>
+	</table>
+	<table>
+		<tr>
+			<th><input name="o" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="p" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="q" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+		</tr>
+		<tr>
+			<th>82</th>
+			<th>31</th>
+			<th>51</th>
+		</tr>
+	</table>
+	<table>
+		<tr>
+			<th><input name="r" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="s" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+		</tr>
+		<tr>
+			<th>52</th>
+			<th>62</th>
+		</tr>
+	</table>
+	<table>
+		<tr>
+			<th><input name="t" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="u" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="v" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+		</tr>
+		<tr>
+			<th>32</th>
+			<th>52</th>
+			<th>41</th>
+		</tr>
+	</table>
+	<table>
+		<tr>
+			<th><input name="w" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="x" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="y" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="z" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="aa" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="ab" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="ac" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="ad" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+		</tr>
+		<tr>
+			<th>51</th>
+			<th>41</th>
+			<th>61</th>
+			<th>41</th>
+			<th>52</th>
+			<th>62</th>
+			<th>32</th>
+			<th>41</th>
+		</tr>
+	</table>
+	<table>
+		<tr>
+			<th><input name="ae" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="af" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="ag" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="ah" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="ai" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="aj" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="ak" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="al" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="am" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="an" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+			<th><input name="ao" type="text" onkeyup="this.value = this.value.toUpperCase();" maxlength="1" form="submit"></th>
+		</tr>
+		<tr>
+			<th>31</th>
+			<th>91</th>
+			<th>42</th>
+			<th>51</th>
+			<th>41</th>
+			<th>62</th>
+			<th>71</th>
+			<th>61</th>
+			<th>61</th>
+			<th>41</th>
+			<th>62</th>
+		</tr>
+	</table>
+	<form method="post" id="submit">
+		<input type="submit" style="width: 30%; height: 30px;" name="submit" value="Überprüfen" />
+	</form>
+</body>
+</html>
